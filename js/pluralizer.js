1e738e1389a988f114852aca76268fe6c1ed914f
asundby6 <asundby6@yahoo.com>
2023-01-10 07:11:47 -0500

    final commit

--- js/pluralizer.js
+++ js/pluralizer.js
@@ -1,34 +1,138 @@
-const pluralizer = (thing,count) => {
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// /* FUNCTION BLOCK */
+
+// const pluralStrings_Vowel = ((newThing) => {                                                                                   // ENDS 'y' ; vowel <- 'y' ; adds 's'
+//     let things = newThing;
+//         console.log("|*| " + things + " initial store value... \n\tFunction Name :  |pSV| / 'pluralStringsVowel' |*|");
+//     things = things.concat('s');
+//         console.log("|*| " + things + " updated. \n\tUsed Function Name :  |pSV| / 'pluralStringsVowel' |*|");
+//     return things;
+// })
+
+// /*
+// const pluralStrings_Consonant = ((newThing) => {                           // ENDS 'y' ; consonant <- 'y' ; REMOVES 'y' -> ADDS 'ies'
+//     let things = newThing;  
+//         console.log("|*| " + things + " initial store value... \n\tUsing Function Name : |pSC| / 'pluralStrings_Consonant' |*|");
+//     let slice_at_value = things.charAt(things.length-1)                                                             // newThing
+//     things = things.slice(slice_at_value);
+//         console.log("\t\t " + things + "\t\t#*#*#*#*TEMP#*#*#*#*" +"\nthings.slice => ' " +things+ " '" );
+//     console.log("|*| " + things + " updated. \n\tUsing Function Name : |pSC| / 'pluralStrings_Consonant' |*|");
     
-    // used to check the last char in a string
-    const countCheck = thing.charAt(thing.length-1);
+//     return things;
+// })
+// */
 
-    // used when manipulating a string if that string ends in 'y'
-    const newChar = thing.slice(0,thing.length-1);
+// const pluralStrings_Default = ((newThing) => {                                                                                                  // NO 'y' ; adds 's'
+//     let things = newThing;
+//         console.log("|*| " + things + " initial store value... \n\tUsing Function Name :  |pSD| / 'pluralStringsDefault' |*|");
+//     things = things.concat('s');
+//         console.log("|*| " + things + " updated. \n\tUsed Function Name :  |pSD| / 'pluralStringsDefault' |*|");
+//     return things;
+// })
+
+
+// /* MAIN BLOCK */
+
+// const thing = ["cat", "kitty", "dog", "puppy"];
+// checkElementOne = 'y'
+// checkElementTwo = ['a', 'e', 'i', 'o', 'u']   
+
+// const whatToDo = (things) => { 
     
-    const y = "y"
+//     let totalLength = things.length;
 
-    if(count > 1 && newChar.includes(y)){                  
-        console.log(count + '' + newChar + 'ies.');
-    }
+// //checking
+//     console.log('|*|*|*|*|*||*|*|*|*|*||*|*|*|*|*||*|*|*|*|*||*|*|*|*|*||*|*|*|*|*||*|*|*|*|*||*|*|*|*|*||*|*|*|*|*||*|*|*|*|*||*|*|*|*|*||*|*|*|*|*|');
 
-    else if(count > 1 && thing.countCheck !== 'y'){
-        console.log(count + ' ' + thing + 's.');
-    }
+//     console.log("\t  things.charAt().totalLength ")
+//     console.log(things.charAt().totalLength);
 
-    else{
-        console.log(count + ' ' + thing + '.');
-    }
+//     // console.log("");
+
+
+//         //  checks for 'y'            **            checks for ${val} <- 'y'            **            consonant  {others} -> 'y'
+//         if (things.charAt(totalLength) == ('y') && (things.charAt((totalLength).includes(checkElementTwo) = true))){                                                                                                                         
+//             console.log()
+//             let pSV = pluralStrings_Vowel(things);
+//         }
+//         // searching similarly to ("  ^ 'same as above' ^  ")   ;   except desired outcomes = removing the 'y' and adding 'ies'
+//         else if ((things.charAt(totalLength) == "y") && (things.charAt(totalLength-1).includes(checkElementOne) == true)) {                                                                                                                         
+//             let pSC = pluralStrings_Consonant(things);
+//         }
+//         //            ('default')     |~V~|    Default Pluralizer Case    |~V~|            ('default')                             
+//         else {       
+//             let tempVal = things.charAt(things.length);                   
+//             let tempVal2 = things.charAt((totalLength));                                                                                             
+//             console.log("\t\t " + tempVal + "\n " + totalLength);
+//             console.log(" things.charAt(totalLength) " + tempVal2);
+            
+//             let pSD = pluralStrings_Default(things);
+//         } 
+
+// }   
+
+//         //-|---------------------------------------------------------------------------------------------|
+//         //~(func)|         thing.forEach((things = thing.slice(",").map(whatToDo)) => {       (func)~\\  |_             
+//         //-|-----------------------------------------------------------------------------------------------|_
+//         // | LOOPS 'forEach' |=>|   { :item: 'things' }  =  { :itemOther: 'thing'}.slice(',').map(whatToDo) ~}
+//         // |#|~things : represents the individual strings in between each ',' : <-->> {'~' + ('~').slice} ~|#|   
+//         // |#|~({'~' + '~'}).map(whatToDo)->the new array formed from map is told to be pass as a (param) ~|#|
+//         // |------------------------------------------------------------------------------------------------|
+
+// thing.forEach((things = thing.slice(",").map(whatToDo)) => {                 
+//     let parameterSingle = things;
+//     let stringDestination = whatToDo(parameterSingle);
+//     return stringDestination;
+// });   
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // Functions (to be called) \\
+
+
+
+
+
+
+            //      Main (driver)      \\
+
+            
+            
+const whatToDo = (singleString) => {
+                
+    // console.log("Single Strings From Array : \n");
+    // console.log("\t=> " + singleString)
+
+
+    let stringLength = singleString.length;
+
+
+    console.log("singleString : " +  singleString + "\n\tString Length : " + stringLength);
+
+    // if () {
+
+    //     console.log();
+    
+    // }
+
 
 }
 
-pluralizer('cat',5);
-pluralizer('kitty',2);
-pluralizer('cat',1);
 
-/*
-and outputs the pluralized form of the word, depending on what count is. 
-    For example, "5 cats" or "1 dog". Change thing to different values to test your code!
-Requirements
-Your program should pluralize the word based on an integer (count)
-*/
+const thing = ["cat", "kitty", "dog", "puppy"];
+            
+thing.forEach((things = thing.slice(",").map(whatToDo)) => {                 
+    let parameterSingle = things;
+    let stringDestination = whatToDo(parameterSingle);
+
+}); 
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
